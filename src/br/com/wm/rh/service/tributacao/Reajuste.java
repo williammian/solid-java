e479c2bd4c57f850c1f1e9e7be9b3554eb551d96
William Mian <william_mian@yahoo.com.br>
2022-01-05 10:40:49 -0300

    DIP - ISP

---
+++ src/br/com/wm/rh/service/tributacao/Reajuste.java
@@ -0,0 +1,11 @@
+package br.com.wm.rh.service.tributacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface Reajuste {
+	
+	BigDecimal valor();
+	LocalDate data();
+	
+}
---
+++ src/br/com/wm/rh/service/tributacao/ReajusteTributavel.java
@@ -0,0 +1,9 @@
+package br.com.wm.rh.service.tributacao;
+
+import java.math.BigDecimal;
+
+public interface ReajusteTributavel extends Reajuste {
+	
+	BigDecimal valorImpostoDeRenda();
+
+}
